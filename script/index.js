38b1a8081bf7728d5ca2457ec800f0b8eb33d93b
jackual <162829405+jackual@users.noreply.github.com>
2024-05-03 22:40:10 +0100

    mouse animation fix

--- script/index.js
+++ script/index.js
@@ -2,22 +2,28 @@ const images = {
     map(fn) {
         return [...document.querySelectorAll("use")].map(fn)
     },
-    regex(use) {
+    regex(use, isOriginal = false) {
         const original = use.attributes.transform.value,
             regex = original.match(/translate\(([-0-9.]+) ([-0-9.]+)/),
             x = parseFloat(regex[1]),
             y = parseFloat(regex[2]),
-            dataX = parseFloat(use.getAttribute("data-x")),
-            dataY = parseFloat(use.getAttribute("data-y"))
+            dataX = parseFloat(use.getAttribute("data-x" + (isOriginal ? "-original" : ""))),
+            dataY = parseFloat(use.getAttribute("data-y" + (isOriginal ? "-original" : "")))
         return { x, y, dataX, dataY }
     },
     stamp(use) {
-        this.map(i => {
-            const coords = this.regex(i)
-            Object.entries(coords).map(x => {
-                i.setAttribute("data-" + x[0], x[1])
-            })
+        const stretchHeight = (window.innerHeight / (window.innerWidth / 1920))
+        document.querySelector("svg").setAttribute("viewBox", `0 0 1920 ${(stretchHeight)}`)
+        //might need to above if more than one svg
+        images.map(i => {
+            const coords = images.regex(i, true)
+            coords.y = coords.y * (stretchHeight / 1080)
+            i.setAttribute("data-x", coords.x)
+            i.setAttribute("data-y", coords.y)
+            images.move(i, () => coords)
+
         })
+
     },
     move(use, fn) {
         const data = this.regex(use)
@@ -45,21 +51,27 @@ onload = async () => {
                 .querySelector("svg")
         })
     document.getElementById("bg").appendChild(svg)
-    images.stamp()
     images.map((image, j) => {
         image.style.display = "none"
+        const coords = images.regex(image)
+        image.setAttribute("data-x-original", coords.x)
+        image.setAttribute("data-y-original", coords.y)
         setTimeout(() => {
             image.style.display = "unset"
-        }, j * 30)
+        }, j * 5)
     })
+    images.stamp()
 }
 
 onmousemove = mouse => {
     images.moveAll((data, element) => {
         const clientRect = element.getBoundingClientRect()
         const div = element.getAttribute("xlink:href") == "#image-2" ? 1000 : 3000
-        const distanceY = ((mouse.clientY - data.dataY) ** 2) / div//2.086956
-        return { x: data.dataX, y: data.dataY - distanceY }
+        const distanceY = ((mouse.clientY - data.dataY) ** 2) / div //2.086956
+        const output = { x: data.dataX, y: data.dataY - distanceY }
+        console.log(data.dataY, distanceY, mouse.clientY, distanceY, output.y)
+        console.log(data, output)
+        return output
         //get centre of element
         //calculate distance to mouse pointer
         //find percent of total screen width
