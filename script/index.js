831c7684a0505fb8eaa832e0c24a6b755a3c7344
jackual <162829405+jackual@users.noreply.github.com>
2024-05-22 20:37:58 +0100

    research page

--- script/index.js
+++ script/index.js
@@ -106,66 +106,79 @@ onload = async () => {
             onload: () => {
                 const data = [
                     {
-                        "name": "Activity",
-                        "desc": "Activity-passivity: In a two-dimensional and static space dynamism or the sensation of movement can be achieved (through diagonals, distortions, saturated colours or typographical contrast): in short, activity. At the other extreme, passivity seeks to transmit a sensation of absolute repose. It is usually linked to techniques with similar objectives, like neutrality, reticence or economy, among others.",
-                        "img": "img/charts/activity.png"
+                        name: 'Activity',
+                        desc: 'Activity-passivity: In a two-dimensional and static space dynamism or the sensation of movement can be achieved (through diagonals, distortions, saturated colours or typographical contrast): in short, activity. At the other extreme, passivity seeks to transmit a sensation of absolute repose. It is usually linked to techniques with similar objectives, like neutrality, reticence or economy, among others.',
+                        img: 'img/charts/activity.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Activity (%)</th><th>Passivity</th><th>Activity</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>37</td><td>63</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>68</td><td>32</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Balance",
-                        "desc": "Symmetry-asymmetry: These are really two formulas to represent balance. While symmetrical balance is the static formula par excellence, asymmetry is obtained when the elements move away from the axis and give way to new relationships of more dynamic forces. Symmetry and asymmetry (as alternatives therefore to static and dynamic equilibrium respectively) and instability (the absence of balance) constitute therefore three options for representing the distribution of forces from levelling or sharpening.",
-                        "img": "img/charts/balance.png"
+                        name: 'Balance',
+                        desc: 'Symmetry-asymmetry: These are really two formulas to represent balance. While symmetrical balance is the static formula par excellence, asymmetry is obtained when the elements move away from the axis and give way to new relationships of more dynamic forces. Symmetry and asymmetry (as alternatives therefore to static and dynamic equilibrium respectively) and instability (the absence of balance) constitute therefore three options for representing the distribution of forces from levelling or sharpening.',
+                        img: 'img/charts/balance.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Balance (%)</th><th>Symmetry</th><th>Asymmetry</th><th>Instability</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>52.5</td><td>47.5</td><td>0</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>12</td><td>54</td><td>34</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Coherence",
-                        "desc": "Coherence-variation: In coherence, the elements are governed by one and the same formal criterion and some uniform visual features. Its opposite, variation, is the technique whose elements admit original changes and modifications.",
-                        "img": "img/charts/coherence.png"
+                        name: 'Coherence',
+                        desc: 'Coherence-variation: In coherence, the elements are governed by one and the same formal criterion and some uniform visual features. Its opposite, variation, is the technique whose elements admit original changes and modifications.',
+                        img: 'img/charts/coherence.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Coherence</th><th>Coherence</th><th>Variation</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>29</td><td>10</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>15</td><td>5</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Continuity",
-                        "desc": "Continuity-episodicity: Continuity is defined by the existence of links and connections between the elements. In episodicity there is no proximity between the pieces, whose individual nature is reinforced by the dispersion of the parts.",
-                        "img": "img/charts/countinuity.png"
+                        name: 'Continuity',
+                        desc: 'Continuity-episodicity: Continuity is defined by the existence of links and connections between the elements. In episodicity there is no proximity between the pieces, whose individual nature is reinforced by the dispersion of the parts.',
+                        img: 'img/charts/countinuity.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Continuity</th><th>Continuity</th><th>Episodicity</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>34</td><td>4</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>16</td><td>4</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Depth",
-                        "desc": "Depth-flatness: On two-dimensional surfaces depth offers a three-dimensional sense, while flatness refers to the total absence of perspective in a composition. They are usually the basic criteria of visual trends such as skeuomorphism and flat design, respectively.",
-                        "img": "img/charts/flatness.png"
+                        name: 'Depth',
+                        desc: 'Depth-flatness: On two-dimensional surfaces depth offers a three-dimensional sense, while flatness refers to the total absence of perspective in a composition. They are usually the basic criteria of visual trends such as skeuomorphism and flat design, respectively.',
+                        img: 'img/charts/flatness.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Depth</th><th>Flatness</th><th>Depth</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>32</td><td>7</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>12</td><td>2</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Greyscale",
-                        "desc": "Use of greyscale colour palette.",
-                        "img": "img/charts/greyscale.png"
+                        name: 'Greyscale',
+                        desc: 'Use of greyscale colour palette.',
+                        img: 'img/charts/greyscale.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Greyscale</th><th>Greyscale</th><th>Colour</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>0</td><td>100</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>36</td><td>64</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Regularity",
-                        "desc": "Regularity-Irregularity: Compositional regularity is based on uniformity and homogeneity of the elements which make up the piece. Visual resolution is organised, following a pre-established pattern, and usually stems from a uniform space via a clearly defined grid. Irregularity is based, on the other hand, on diversity and on a visible plan of distribution to reinforce the surprising, unusual and dynamic feature.",
-                        "img": "img/charts/regularity.png"
+                        name: 'Regularity',
+                        desc: 'Regularity-Irregularity: Compositional regularity is based on uniformity and homogeneity of the elements which make up the piece. Visual resolution is organised, following a pre-established pattern, and usually stems from a uniform space via a clearly defined grid. Irregularity is based, on the other hand, on diversity and on a visible plan of distribution to reinforce the surprising, unusual and dynamic feature.',
+                        img: 'img/charts/regularity.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Regularity</th><th>Regularity</th><th>Irregularity</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>32</td><td>8</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>5</td><td>20</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Sequentiality",
-                        "desc": "Sequentiality-randomness: Sequentiality seeks to establish a logical order or rhythmic pattern from the elements and is usually used to explain developments or changes. In a given visual composition it is a synonym of hierarchy. Randomness, however, suggests a certain disorganisation of the elements.",
-                        "img": "img/charts/sequentiality.png"
+                        name: 'Sequentiality',
+                        desc: 'Sequentiality-randomness: Sequentiality seeks to establish a logical order or rhythmic pattern from the elements and is usually used to explain developments or changes. In a given visual composition it is a synonym of hierarchy. Randomness, however, suggests a certain disorganisation of the elements.',
+                        img: 'img/charts/sequentiality.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Sequentiality</th><th>Sequentiality</th><th>Randomness</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>32</td><td>6</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>3</td><td>11</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Sharpness",
-                        "desc": "Sharpness-diffusiveness: Sharpness strengthens the outlines and fixes the composition through definition, clarity and precision of the units’ features. Diffusiveness aims for a softer or more blurred definition of the elements.",
-                        "img": "img/charts/sharpness.png"
+                        name: 'Sharpness',
+                        desc: 'Sharpness-diffusiveness: Sharpness strengthens the outlines and fixes the composition through definition, clarity and precision of the units’ features. Diffusiveness aims for a softer or more blurred definition of the elements.',
+                        img: 'img/charts/sharpness.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Sharpness</th><th>Sharpness</th><th>Diffusiveness</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>24</td><td>14</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>4</td><td>3</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Simplicity",
-                        "desc": "Simplicity-complexity: Simplicity seeks an immediate, clear and direct solution, free of superfluous elements and useless additions. Complexity, on the other hand, is created through the use of numerous units and elements (usually) and from an implausible structure which gives rise to intricate visual schemata.",
-                        "img": "img/charts/simplicity.png"
+                        name: 'Simplicity',
+                        desc: 'Simplicity-complexity: Simplicity seeks an immediate, clear and direct solution, free of superfluous elements and useless additions. Complexity, on the other hand, is created through the use of numerous units and elements (usually) and from an implausible structure which gives rise to intricate visual schemata.',
+                        img: 'img/charts/simplicity.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Simplicity (%)</th><th>Simplicity</th><th>Complexity</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>63</td><td>37</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>66</td><td>34</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Singularity",
-                        "desc": "Singularity-juxtaposition: Singularity is a composition based on the absolute protagonism of one of its components which eclipses the others and forms the main point of the message. Juxtaposition, for its part, is based on the complementarity of two or more of the composition’s units which contain similar relevance and acquire a mutual dependency in order to give meaning to the image.",
-                        "img": "img/charts/singularity.png"
+                        name: 'Singularity',
+                        desc: 'Singularity-juxtaposition: Singularity is a composition based on the absolute protagonism of one of its components which eclipses the others and forms the main point of the message. Juxtaposition, for its part, is based on the complementarity of two or more of the composition’s units which contain similar relevance and acquire a mutual dependency in order to give meaning to the image.',
+                        img: 'img/charts/singularity.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Singularity</th><th>Singularity</th><th>Juxtaposition</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>26</td><td>12</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>6</td><td>4</td>      </tr>  </tbody></table>'
                     },
                     {
-                        "name": "Webfonts",
-                        "desc": "Use of external webfonts.",
-                        "img": "img/charts/webfonts.png"
+                        name: 'Webfonts',
+                        desc: 'Use of external webfonts.',
+                        img: 'img/charts/webfonts.png',
+                        table: '<table class="interactive">  <thead>      <tr>          <th>Webfonts</th><th>Webfonts</th><th>System Fonts</th>      </tr>  </thead>  <tbody>      <tr>      <td>Alexa Ranking</td><td>85</td><td>15</td>      </tr>      <tr>      <td>Brutalist Websites</td><td>28</td><td>72</td>      </tr>  </tbody></table>'
                     }
-                ];
+                ]
+
 
                 console.log(data.length);
 
@@ -210,6 +223,7 @@ onload = async () => {
                     img.setAttribute("src", row.img)
                     img.setAttribute("alt", `${row.name} Graph`)
                     document.querySelector("#vcs textarea").value = row.desc
+                    document.querySelector("#tt").innerHTML = row.table
                 }
 
                 select.onchange = () => { updateVcs(select.value) }
